922e74ae847114afe32a2241bfe6554260b4ed30
Your Name <you@example.com>
2025-01-22 12:24:54 +0530

    sidenavbar

---
+++ src/utils/helper.js
@@ -0,0 +1,23 @@
+export const LINKS = [
+    {
+        title: "Home",
+        path: "/"
+    },
+    {
+        title: "About",
+        path: "/about"
+    },
+    {
+        title: "Product",
+        path: "/product"
+    },
+    {
+        title: "Docs",
+        path: "/docs"
+    },
+    {
+        title: "Help",
+        path: "/help"
+    },
+
+]
